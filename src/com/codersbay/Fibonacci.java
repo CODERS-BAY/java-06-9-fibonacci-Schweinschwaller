7f7d577cfdc94be8e48391f076a61580308a1bb0
Georg Schweinschwaller <georg.schweinschwaller@live.at>
2020-09-14 15:09:46 +0200

    Add Fibonacci recursiv + Methoden Variante for long

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -1,9 +1,34 @@
 package com.codersbay;
 
-public class Fibonacci {
+import java.util.Scanner;
 
+public class Fibonacci {
     public static void main(String[] args) {
+
         System.out.println("Hello Fibonacci");
+        System.out.print("n-te Fibonacci Zahl:");
+        Scanner s = new Scanner(System.in);
+        int input = s.nextInt();
+        System.out.println();
+        if (input < 46) {
+            System.out.print(fibonacci(input, 1, 1, 1));
+        } else {
+            System.out.print(fibonacci(input, (long) 1, (long) 1, 1));
+        }
+        System.out.println();
     }
 
+    public static int fibonacci(int border, int number1, int number2, int run) {
+        if (border == run) {
+            return number2;
+        }
+        return fibonacci(border, number1 + number2, number1, ++run);
+    }
+
+    public static long fibonacci(int border, long number1, long number2, int run) {
+        if (border == run) {
+            return number2;
+        }
+        return fibonacci(border, number1 + number2, number1, ++run);
+    }
 }
